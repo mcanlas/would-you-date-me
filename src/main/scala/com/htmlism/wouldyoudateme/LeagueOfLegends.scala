bbd2cb58be4f2a0acd0039ee527780d6bd8a4c9a
Mark Canlas <github@htmlism.com>
2020-08-10 20:11:37 -0400

    new split

--- src/main/scala/com/htmlism/wouldyoudateme/LeagueOfLegends.scala
+++ src/main/scala/com/htmlism/wouldyoudateme/LeagueOfLegends.scala
@@ -5,14 +5,15 @@ import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
 object LeagueOfLegends extends App {
-  val maxPoints = 500
+  val maxPoints = 750
 
   val now = LocalDate.now()
   val endDate = LocalDate.of(2020, 8, 9)
+    .plusDays(107L)
 
   val hours = ChronoUnit.HOURS.between(now.atStartOfDay(), endDate.atStartOfDay())
 
-  val already = args.headOption.map(_.toInt).getOrElse(80)
+  val already = args.headOption.map(_.toInt).getOrElse(0)
 
   for (n <- 0 to (maxPoints - already) by 20) {
     val newDate = now.atStartOfDay().plusHours(hours * n / maxPoints)
